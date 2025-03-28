4b94f7a84900bbfd23d802e3ca474500030ed617
Albert Bodenhamer <albert@readers-indigestion.com>
2025-03-28 16:55:25 -0700

    Flipping through pages now works

--- test1.asm
+++ test1.asm
@@ -1,85 +1,62 @@
+
+            * = $0400 "Bank0"
+bank0:
+            .text "bank 0                                  "
+            .text "press any key to switch banks           "
+            .text "                                        "
+            .fill 256,i  // Write out the full character set.
+            .fill 624,'0'
+
 BasicUpstart2(start)
-			* = $1000
-start: 		sei // Disable interrupts
+        	* = $2000 "Code"
+            .const PAGE_SELECT = $DD00
+            .const CURRENT_PAGE = $02
+start: 		
+            // There are 2 page select bits in the bottom
+            // 2 bits of $DD00. They are active LOW, so
+            // Page 0 = both bits on, Page 1 = bottom bit off, etc.
+            // Init our current page to page 0 (#3) 
+            lda #3
+            sta CURRENT_PAGE
+loop:
+            lda PAGE_SELECT
+            and #$FC
+            ora CURRENT_PAGE
+            sta PAGE_SELECT
+poll:       jsr $FFE4        // Calling KERNAL GETIN 
+            beq poll
+            dec CURRENT_PAGE
+            // If we decrement past 0, jump back to the beginning and init back
+            // to page 0
+            lda #$FF
+            cmp CURRENT_PAGE
+            beq start
+            jmp loop
+exit: // TODO create a way to get here
+            rts  // Return to basic
 
-			jsr $e544 // Clear screen
-			// lda #$18 // bank stuff - TODO
-			// sta $d018
-			
-            // Store the current screen bank in $45
-            .const current_bank = $45
-            lda #0
-            sta current_bank
+// Todo load character set for banks 1 and 3
 
-			// The start of screen ram is at $0400
-            .const target_high = $04
-            .const target_low = $00
-            // Store our write location in the zero page.
-            // $43 and $44 are suppsedly used by the BASIC INPUT routine
-            // Since we're not using that they should be safe.
-            .const bankstart = $43;
-            lda #target_low
-            sta bankstart
-            lda #target_high
-            sta bankstart + 1
-bankfillloop:
-			ldy #0
-headerloop:	lda header,y // Grab char y from header into register a
-			cmp #$ff   // Look for $ff
-			beq headerexit   // Exit the loop when we see $ff
-			sta (bankstart),y // Store register a into target + y
-			iny // increment y
-			jmp headerloop // jump back to the start of the loop
-headerexit:
-            // Write out the bank number
-            ldx current_bank
-            lda numbers, x
-            sta (bankstart), y
+            * = $4400 "Bank1"
+bank1:
+            .text "bank 1                                  "
+            .text "press any key to switch banks           "
+            .text "                                        "
+            .fill 256,i  // Write out the full character set.
+            .fill 624,'1'
 
-            // Skip down 3 rows before writing the character set to the screen.
-            clc
-            lda #120
-            adc bankstart
-            sta bankstart
+            * = $8400 "Bank2"
+bank2:
+            .text "bank 2                                  "
+            .text "press any key to switch banks           "
+            .text "                                        "
+            .fill 256,i  // Write out the full character set.
+            .fill 624,'2'
 
-            ldy #0
-charloop:   tya    // Store y in A
-            sta (bankstart),y
-            iny
-            cpy #0 // Compare y to 0. It's only 8 bits and will wrap.
-            beq charexit
-            jmp charloop
-
-charexit:
-            // Add $40 to the high byte of bankstart to increment our
-            // write location by 16k.
-            clc
-            lda #$40
-            adc bankstart + 1
-            sta bankstart + 1
-
-            // Clear low byte of bankstart to start writing at top of screen.
-            lda #0
-            sta bankstart
-
-            // Increment the current bank
-            clc
-            lda #1
-            adc current_bank
-            cmp #4
-            beq exit
-            sta current_bank
-            jmp bankfillloop
-
-exit:
-            lda $DD00
-            and #%11111100
-            ora #%00000011
-            sta $DD00
-
-			rts // Return to BASIC
-header: 	.text "bank "
-			.byte $ff
-numbers:    .text "0123456789"
-
-			
+            * = $C400 "Bank3"
+bank3:
+            .text "bank 3                                  "
+            .text "press any key to switch banks           "
+            .text "                                        "
+            .fill 256,i  // Write out the full character set.
+            .fill 624,'3'
