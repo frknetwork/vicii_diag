1845e2f232c37a7b2ae3f07e03daed6b854160e4
Albert Bodenhamer <albert@readers-indigestion.com>
2025-03-28 17:12:37 -0700

    Save. Checkpoint on copying character rom

--- test1.asm
+++ test1.asm
@@ -1,3 +1,7 @@
+            * = $80 "ZP"
+crom:       .dword $1000 // Store the location of the character ROM in ZP
+cdest1:     .dword $5000 // Character ROM location for Bank 1
+cdest3:     .dword $D000 // Character ROM location for Bank 3
 
             * = $0400 "Bank0"
 bank0:
@@ -12,11 +16,19 @@ BasicUpstart2(start)
             .const PAGE_SELECT = $DD00
             .const CURRENT_PAGE = $02
 start: 		
+            // Copy character ROM to locations where banks
+            // 1 and 3 can access the data.
+            ldy #$FF         
+cloop:      lda ($80),y    
+            sta $0400,y    
+            dey            
+            bpl cloop      
+
             // There are 2 page select bits in the bottom
             // 2 bits of $DD00. They are active LOW, so
             // Page 0 = both bits on, Page 1 = bottom bit off, etc.
             // Init our current page to page 0 (#3) 
-            lda #3
+init:       lda #3
             sta CURRENT_PAGE
 loop:
             lda PAGE_SELECT
@@ -30,7 +42,7 @@ poll:       jsr $FFE4        // Calling KERNAL GETIN
             // to page 0
             lda #$FF
             cmp CURRENT_PAGE
-            beq start
+            beq init
             jmp loop
 exit: // TODO create a way to get here
             rts  // Return to basic
