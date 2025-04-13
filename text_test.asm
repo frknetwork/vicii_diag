fecdf1e150c4485c7d3e609c210b62da3ab5aca2
Albert Bodenhamer <albert@readers-indigestion.com>
2025-04-13 14:08:08 -0700

    Lots more use of constants and fiddling with indentation.

--- text_test.asm
+++ text_test.asm
@@ -1,62 +1,67 @@
-// Set up pointers in ZP
-.var src = $43
-.watch src
-.var dest1 = $45
-.watch dest1
-.var dest3 = $FD
-.watch dest3
+// Copyright 2025 Freaking Rad Kreative Network
+// For terms see the LICENSE file in this project.
 
-.const charrom = $D000
-.const char1 = $5000
-.const char3 = $D000
-.const charset_size = $1000
+// Set up pointers in ZP
+.const SRC_PTR = $43
+.watch SRC_PTR
+.const DEST_1_PTR = $45
+.watch DEST_1_PTR
+.const DEST_3_PTR = $FD
+.watch DEST_3_PTR
+
+.const CHAR_ROM = $D000
+.const CHAR_BANK_1 = $5000
+.const CHAR_BANK_3 = $D000
+.const CHAR_ROM_SIZE = $1000
+.const CPU_REGISTER_1 = $1
+.const CPU_CHAR_ROM_MASK = $FB
 
 .macro SetupCharsets() {
     // Copy character ROM to locations where banks
     // 1 and 3 can access the data.
     // Set up pointers to the data we want to copy in ZP
     lda #0
-    sta src
-    sta dest1
-    sta dest3
-    lda #>charrom
-    sta src + 1
-    lda #>char1
-    sta dest1 + 1
-    lda #>char3
-    sta dest3 + 1
+    sta SRC_PTR
+    sta DEST_1_PTR
+    sta DEST_3_PTR
+    lda #>CHAR_ROM
+    sta SRC_PTR + 1
+    lda #>CHAR_BANK_1
+    sta DEST_1_PTR + 1
+    lda #>CHAR_BANK_3
+    sta DEST_3_PTR + 1
 
     sei // Disable interrupts
-    
+
     // Map the character ROM into CPU readable space
-    lda 1
-    and #$FB
-    sta 1
+    lda CPU_REGISTER_1      // Read the register
+    and #CPU_CHAR_ROM_MASK  // Zero out the bit
+    sta CPU_REGISTER_1      // Write it back
 
     // Do the actual copy
-    ldy #0 // Start at offset 0
+    ldy #0
 loop:       
-    lda (src),y // Read a byte
-    sta (dest1),y // Write it to bank 1
-    sta (dest3),y // Write it to bank 3
-    iny // move to the next byte
-    bne loop // Loop until the index rolls back to 0
-    
+    lda (SRC_PTR),y         // Read a byte
+    sta (DEST_1_PTR),y      // Write it to bank 1
+    sta (DEST_3_PTR),y      // Write it to bank 3
+    iny                     // move to the next byte
+    bne loop                // Loop until the index rolls back to 0
+
     // Increment the high byte of each of our pointers
-    inc src + 1
-    inc dest1 + 1
-    inc dest3 + 1
-    
+    inc SRC_PTR + 1
+    inc DEST_1_PTR + 1
+    inc DEST_3_PTR + 1
+
     // Finish once we've copied the full character set
-    lda #>(char3+charset_size)
-    cmp src + 1
+    lda #>(CHAR_BANK_3+CHAR_ROM_SIZE)
+    cmp SRC_PTR + 1
     bne loop 
 
     // Map the character ROM back out of CPU space so we can
     // access IO registers
-    lda 1
-    ora #$04
-    sta 1
+    lda CPU_REGISTER_1
+    ora #~CPU_CHAR_ROM_MASK     // Turn the bit back on
+    sta CPU_REGISTER_1
 
     // Re-enable interrupts
     cli
@@ -67,101 +72,119 @@ bankdata:
     .text "press space to switch banks             "
     .text "press f1 to switch character set        "
     .text "                                        "
-    .fill 256,i  // Write out the full character set.
-    .fill 584, 0
-.const chars_per_line = 40
-.const lines = 25
+    .fill 256,i     // Display each character on screen
+    .fill 584, 0    // Pad out with the current bank #
+.const CHARS_PER_LINE = 40
+.const LINES = 25
 
 .macro IncrementPointer(pointer, count) {
-    lda #count
+    lda #count      // Start with the amount to add and no carry
     clc
-    adc pointer
-    sta pointer
-    lda #0
-    adc pointer + 1
-    sta pointer + 1
+    adc pointer     // Add the count to the low byte
+    sta pointer     // Store it back
+    lda #0          // Reset a
+    adc pointer + 1 // Add any carry into the high byte
+    sta pointer + 1 // Store it
 }
 
 .macro SetupBank(fill_char, location) {
     // Set up our source and destination pointers in ZP
     lda #<location
-    sta dest1
+    sta DEST_1_PTR
     lda #>location
-    sta dest1 + 1
+    sta DEST_1_PTR + 1
 
     lda #<bankdata
-    sta src
+    sta SRC_PTR
     lda #>bankdata
-    sta src + 1
+    sta SRC_PTR + 1
 
     ldx #0
 outloop:
     ldy #0
 loop:
-    lda (src),y
-    bne nonzero // If we read a NULL character, write our fill character instead.
-    lda #fill_char
+    lda (SRC_PTR),y     // Read from bankdata
+    bne nonzero         // If we read a NULL character,
+    lda #fill_char      // write our fill character instead.
 nonzero:
-    sta (dest1),y
+    sta (DEST_1_PTR),y  // Store to the target
     iny
-    cpy #chars_per_line
-    bne loop
-    IncrementPointer(dest1, chars_per_line)
-    IncrementPointer(src, chars_per_line)
-    inx
-    cpx #lines
+    cpy #CHARS_PER_LINE
+    bne loop            // Copy lines one at a time
+
+    // Move pointers to the next line.
+    IncrementPointer(DEST_1_PTR, CHARS_PER_LINE)
+    IncrementPointer(SRC_PTR, CHARS_PER_LINE)
+    inx                 // Loop through all of the lines
+    cpx #LINES
     bne outloop
 }
 
-.const bank0 = $0400
-.const bank1 = $4400
-.const bank2 = $8400
-.const bank3 = $C400
+.const BANK_0 = $0400
+.const BANK_1 = $4400
+.const BANK_2 = $8400
+.const BANK_3 = $C400
+.const KERNAL_GETIN = $FFE4
+.const KERNAL_SCINIT = $FF81
+
+// There are 2 bank select bits in the bottom
+// 2 bits of $DD00. They are active LOW, so
+// Bank 0 = both bits on, bank 1 = bottom bit off, etc.
+// Init our current bank to bank 0 (#3)
+// Store the current bank in ZP at address 2
+.const BANK_SELECT = $DD00
+.const BANK_SELECT_MASK = $FC
+.const CURRENT_BANK = $02
+.const SPACE_BAR = $20
+.const F1_KEY = $85
+.const SET_SELECT = $D018
+.const SET_SELECT_MASK = $02
+.const BANK_0_MASK = $03
 
 BasicUpstart2(start)
-start: 		
+start:
     SetupCharsets()
-    SetupBank('0', bank0)
-    SetupBank('1', bank1)
-    SetupBank('2', bank2)
-    SetupBank('3', bank3)
+    SetupBank('0', BANK_0)
+    SetupBank('1', BANK_1)
+    SetupBank('2', BANK_2)
+    SetupBank('3', BANK_3)
 
-
-    // There are 2 bank select bits in the bottom
-    // 2 bits of $DD00. They are active LOW, so
-    // Bank 0 = both bits on, bank 1 = bottom bit off, etc.
-    // Init our current bank to bank 0 (#3) 
-    .const BANK_SELECT = $DD00
-    .const CURRENT_BANK = $02
 init:
-    lda #3
+    lda #BANK_0_MASK        // Start in bank 0
     sta CURRENT_BANK
 loop:
-    lda BANK_SELECT
-    and #$FC
-    ora CURRENT_BANK
-    sta BANK_SELECT
+    lda BANK_SELECT         // Grab the current state of the register
+    and #BANK_SELECT_MASK   // Zero out the bits we care about
+    ora CURRENT_BANK        // Set those bits to what we want
+    sta BANK_SELECT         // Actually set the register
 poll:
-    jsr $FFE4        // Calling KERNAL GETIN 
-    beq poll
-    cmp #$20
+    jsr KERNAL_GETIN        // Read a key into A
+    beq poll                // Keep reading until a key is actually pressed
+    cmp #SPACE_BAR          // Look for spacebar
     bne notspace
-    dec CURRENT_BANK
-    // If we decrement past 0, jump back to the beginning and init back
-    // to bank 0
-    lda #$FF
-    cmp CURRENT_BANK
-    beq init
+    // Switch our current bank tracker to the next bank
+    dec CURRENT_BANK        // Active LOW so dec instead of inc
+    // If we decrement past 0 (bank 3), jump back to the beginning and
+    // init back to bank 0 (#3)
+    bmi init
     jmp loop
 
 notspace:
-    cmp #$85 // F1 key
+    cmp #F1_KEY
     bne notf1
-    lda $D018
-    eor #2
-    sta $D018
+    lda SET_SELECT
+    eor #SET_SELECT_MASK
+    sta SET_SELECT
     jmp loop
 notf1:
+    // Add additional key handling here or fall thru to exit.
 exit:
-    // TODO Clear screen and return to bank 0 and normal font
+    jsr KERNAL_SCINIT       // Reinit the VICII and clear screen
+
+    // Switch back to bank 0
+    lda BANK_SELECT         // Grab the current state of the register
+    and #BANK_SELECT_MASK   // Zero out the bits we care about
+    ora #BANK_0_MASK        // Set those bits to what we want
+    sta BANK_SELECT         // Actually set the register
+
     rts  // Return to basic
